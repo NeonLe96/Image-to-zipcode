458835a20cb329d9fc70ef9b86030c14a639a7e3
Quang "Neon" Le <quang.neon.le@gmail.com>
2019-11-02 19:48:36 -0700

    Update install_windows.sh

--- install_windows.sh
+++ install_windows.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 
-echo RUN THIS AS SOURCE '. SHELLFILE'
 echo Getting PIP
-sudo apt-get update
-sudo apt install --upgrade python3
-sudo apt install python3-pip
-sudo apt-get install python3-tk
 
-echo Pip3 will now setup required modules for python
-pip3 install --upgrade --user geopy
-pip3 install --upgrade --user google-cloud-vision
-pip3 install --upgrade --user pyinstaller
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+py get-pip.py
+
+echo PLEASE RESTART TERMINAL
+
+echo Pip will now setup required modules for python
+pip install --upgrade --user geopy
+pip install --upgrade --user google-cloud-vision
+pip install --upgrade --user pyinstaller
 pip install --upgrade  --user google-cloud-core
 
-echo Make sure pyinstaller is in path
-export APEND=$( find / -name pyinstaller)
-export APENDIR=$(dirname "${APEND}")
-export PATH=$PATH:$(dirname"${}")
-
-pyinstaller --clean --onefile --additional-hooks-dir=./hooks AppGUI.py
-cp ./authen.json ./dist
-
-echo Executable is ready at dist
+py AppGUI.py
