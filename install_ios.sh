76352516663fd254aa3c9988defe961b6d30a56c
Quang "Neon" Le <quang.neon.le@gmail.com>
2019-11-02 18:29:31 -0700

    Update install_ios.sh

--- install_ios.sh
+++ install_ios.sh
@@ -6,12 +6,13 @@ echo Getting PIP
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install python3
+brew install tcl-tk
 
 echo Pip3 will now setup required modules for python
 pip3 install --upgrade --user geopy
 pip3 install --upgrade --user google-cloud-vision
 pip3 install --upgrade --user pyinstaller
-pip install --upgrade  --user google-cloud-core
+pip3 install --upgrade  --user google-cloud-core
 
 echo Make sure pyinstaller is in path
 export APEND=$( find / -name pyinstaller)
